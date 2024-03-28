e0ea2c9348b2d01a2f60a19ce46b2f84db82019e
krishivh <krishiv.haranath@gmail.com>
2024-03-28 15:58:48 -0700

    finished python HW10
    done with python HW10

--- HW10/HW10.py
+++ HW10/HW10.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class TreeNode(object):
 
      def __init__(self, val=0, left=None, right=None):
@@ -12,4 +15,29 @@ class TreeNode(object):
 class HW10:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         queue = collections.deque
+        
+        if root is None:
+            return None
+        
+        queue.append(root)
+        
+        finalList = []
+        
+        while queue is not None:
+            queueSize = len(queue)
+            Level = []
+            for i in range(queueSize):
+                curr = queue.popleft()
+                if curr is not None:
+                    Level.append(curr.val)
+                    queue.append(curr.left)
+                    queue.append(curr.right)
+            if Level is not None:
+                finalList.append(Level)
+                    
+        return finalList
+                
+                
+        
+        
         
